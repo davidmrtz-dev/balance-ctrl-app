19804bfcb33f7856c664bfe2d51de95aa5dc4dc4
David Mtz <davidelimg1@gmail.com>
2023-01-28 21:52:15 -0600

    [FEAT]: Implemented component

---
+++ src/components/routes/PrivateRoute.tsx
@@ -0,0 +1,25 @@
+import { ComponentType } from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { useAuthContext } from '../../context/AuthContext';
+
+type PrivateRouteProps = RouteProps & {
+  backPrevious?: boolean;
+  redirectTo?: string;
+  component?: ComponentType;
+}
+
+export default function PrivateRoute({ children, component: Component, ...rest }: PrivateRouteProps) {
+  const { backPrevious = false, redirectTo = '/signin' } = rest;
+  const auth = useAuthContext();
+
+  return (
+    <Route {...rest} render={(props) => {
+      return auth.isAuthenticated
+        ? children as JSX.Element || (Component &&  <Component {...props} />)
+        : <Redirect to={{
+          pathname: redirectTo,
+          state: { from: props.location, backPrevious }
+        }} />
+    }}/>
+  );
+}
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,16 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from '../components/routes/PrivateRoute';
 import About from './about';
 import Home from './home';
 import Login from './login';
 import NotFound from './not-found';
 
 const Router = () => {
-  return(<Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/about' element={<About />} />
-    <Route path='*' element={<NotFound />} />
-  </Routes>);
+  return(<Switch>
+    {/* <PrivateRoute exact key='home' path='/' >
+      <Home />
+    </PrivateRoute> */}
+    <PrivateRoute exact key='home' path='/' component={Home} />
+    <Route path='/login' component={Login} />
+    <Route path='/about' component={About} />
+    <Route path='*' component={NotFound} />
+  </Switch>);
 };
 
 export default Router;
