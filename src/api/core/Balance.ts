67d85f594b9f1888a8b1d8cb015009de6204d5b2
David Mtz <davidelimg1@gmail.com>
2023-02-06 13:02:50 -0600

    [WIP]: Implemented ep call in dashboard, adding smooth visualizing UI

--- src/api/core/Balance.ts
+++ src/api/core/Balance.ts
@@ -2,6 +2,11 @@ import * as Http from '../Http';
 import { IBalance } from '../../@types';
 
 export const getBalance = async (): Promise<IBalance> => {
-  const result = await Http.get('/api/balances/balance');
-  return result.data;
+  const result = await Http.get('/api/balance', {}, {
+    'access-token': sessionStorage.getItem('authorization:token') || '',
+    client: sessionStorage.getItem('authorization:client') || '',
+    uid: sessionStorage.getItem('authorization:uid') || '',
+  });
+
+  return result.data.balance;
 };
