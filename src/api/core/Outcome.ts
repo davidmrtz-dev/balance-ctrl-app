c0ee07fe0306cfb4af78403986d1d92ab52f6237
David Mtz <davidelimg1@gmail.com>
2023-02-14 12:37:52 -0600

    [ENHANCE]: Applied dry to outcomes core

--- src/api/core/Outcome.ts
+++ src/api/core/Outcome.ts
@@ -1,30 +1,16 @@
 import * as Http from '../Http';
-import { IOutcomes } from '../../@types';
+import { IOutcomes, TransactionType } from '../../@types';
 
-export const getCurrentOutcomes = async ({
+export const getOutcomes = async({
   offset,
-  limit = 5
+  limit = 5,
+  type
 }: {
   offset: number;
   limit?: number;
+  type: TransactionType
 }): Promise<IOutcomes> => {
-  const result = await Http.get('/api/outcomes/current', { limit, offset }, {
-    'access-token': sessionStorage.getItem('authorization:token') || '',
-    client: sessionStorage.getItem('authorization:client') || '',
-    uid: sessionStorage.getItem('authorization:uid') || ''
-  });
-
-  return result.data;
-};
-
-export const getFixedOutcomes = async ({
-  offset,
-  limit = 5
-}: {
-  offset: number;
-  limit?: number;
-}): Promise<IOutcomes> => {
-  const result = await Http.get('/api/outcomes/fixed', { limit, offset }, {
+  const result = await Http.get(`/api/outcomes/${type === 'current' ? 'current' : 'fixed'}`, { limit, offset }, {
     'access-token': sessionStorage.getItem('authorization:token') || '',
     client: sessionStorage.getItem('authorization:client') || '',
     uid: sessionStorage.getItem('authorization:uid') || ''
