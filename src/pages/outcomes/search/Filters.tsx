d1ef200edc78e0167de1cd029126c2613510b087
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-23 17:14:43 -0600

    [FEAT]: Finished splitting and enhanced components

--- src/pages/outcomes/search/Filters.tsx
+++ src/pages/outcomes/search/Filters.tsx
@@ -1,10 +1,8 @@
 import { Button, DatePicker, Select } from "antd";
 import styled from "styled-components";
-import { TransactionType } from "../../../@types";
 import { theme } from "../../../Theme";
 
 const { RangePicker } = DatePicker;
-type Selector =  TransactionType  | '';
 
 const FiltersContainer = styled.div<{ visible: boolean }>`
   background-color: ${p => p.theme.colors.grays.light};
@@ -35,38 +33,21 @@ export const Filters = ({
     allowClear
     onCalendarChange={(values) => console.log(values)}
     />
-    <TypeSelector onSelect={() => {}} clearSelect={() => {}} disabled={false} />
-    <Button type='primary'>Apply</Button>
-</FiltersContainer>;
-
-const TypeSelectorWrapper = styled.div`
-  background-color: ${p => p.theme.colors.grays.light};
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding: 10px 0;
-`;
-
-const TypeSelector = ({
-  onSelect,
-  clearSelect,
-  disabled
-}: {
-  onSelect: (text: Selector) => void
-  clearSelect: () => void;
-  disabled: boolean;
-}): JSX.Element => <TypeSelectorWrapper>
   <Select
     allowClear
-    onClear={clearSelect}
-    disabled={disabled}
+    onClear={() => {}}
+    disabled={false}
     placeholder={'Type'}
-    style={{ backgroundColor: theme.colors.grays.light, flex: 3 }}
+    style={{
+      width: '100%',
+      padding: '10px 0'
+    }}
     dropdownStyle={{ backgroundColor: theme.colors.grays.light }}
-    onSelect={onSelect}
+    onSelect={() => {}}
     options={[
       { value: 'current', label: 'Current' },
       { value: 'fixed', label: 'Fixed' }
     ]}
   />
-</TypeSelectorWrapper>;
+  <Button type='primary'>Apply</Button>
+</FiltersContainer>;
