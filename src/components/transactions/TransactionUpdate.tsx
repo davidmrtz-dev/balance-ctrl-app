58ec7351e82a1117cdc353aeac8bb4fe4fe52728
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-23 21:46:11 -0600

    [FEAT]: Added prop and display delete btn.

--- src/components/transactions/TransactionUpdate.tsx
+++ src/components/transactions/TransactionUpdate.tsx
@@ -1,5 +1,5 @@
 import { Button, Modal, Typography } from "antd";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IOutcome } from "../../@types";
 import { updateOutcome } from "../../api/core/Outcome";
 import { theme } from "../../Theme";
@@ -10,17 +10,19 @@ export const TransactionUpdate = ({
   outcome,
   open,
   closeModal,
-  handleUpdate
+  handleUpdate,
+  handleDelete
 }: {
   outcome: IOutcome;
   open: boolean;
   closeModal: () => void;
   handleUpdate: (outcome: IOutcome) => Promise<void>;
+  handleDelete?: (outcome: IOutcome) => void;
 }): JSX.Element => {
   const [loading, setLoading] = useState(false);
   const [values, setValues] = useState<IOutcome>({} as IOutcome);
 
-  const handleSubmit = async() => {
+  const handleSubmit = useCallback(async() => {
     if (Object.values(values).some(val => val === '')) {
       Alert({
         icon: 'error',
@@ -51,7 +53,7 @@ export const TransactionUpdate = ({
         closeModal();
       }, 1000);
     }
-  };
+  }, [closeModal, handleUpdate, values]);
 
   const handleCancel = () => {
     setValues({} as IOutcome);
@@ -61,6 +63,40 @@ export const TransactionUpdate = ({
   useEffect(() => {
     setValues(outcome);
   }, [outcome]);
+
+  const footerComponents = [
+    <Button key="cancel" onClick={handleCancel} disabled={loading}>
+      <Typography.Text style={{ ...theme.texts.brandFont }}>
+        Cancel
+      </Typography.Text>
+    </Button>,
+    <Button key="submit" type="primary" loading={loading} onClick={handleSubmit}>
+      <Typography.Text
+        style={{ ...theme.texts.brandFont, color: theme.colors.whites.normal }}
+      >
+        Update
+      </Typography.Text>
+    </Button>
+  ];
+
+  if (handleDelete) {
+    footerComponents.push(<Button style={{
+      backgroundColor: theme.colors.warning
+    }}
+      key="delete"
+      loading={loading}
+      onClick={() => {}}
+    >
+    <Typography.Text
+      style={{
+        ...theme.texts.brandFont,
+        color: theme.colors.whites.normal
+      }}
+    >
+      Delete
+    </Typography.Text>
+  </Button>);
+  }
 
   return (
     <Modal
@@ -76,20 +112,7 @@ export const TransactionUpdate = ({
         maxWidth: 360,
         position: 'relative'
       }}
-      footer={[
-        <Button key="cancel" onClick={handleCancel} disabled={loading}>
-          <Typography.Text style={{ ...theme.texts.brandFont }}>
-            Cancel
-          </Typography.Text>
-        </Button>,
-        <Button key="submit" type="primary" loading={loading} onClick={handleSubmit}>
-          <Typography.Text
-            style={{ ...theme.texts.brandFont, color: theme.colors.whites.normal }}
-          >
-            Update
-          </Typography.Text>
-        </Button>
-      ]}
+      footer={footerComponents}
     >
       <>
         <TransactionForm
