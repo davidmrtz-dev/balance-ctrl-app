07a9567ed0d44608e2d09a1b775201aa18e378d6
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-25 18:58:29 -0600

    [WIP]: Displaying incomes list

--- src/pages/incomes/index.tsx
+++ src/pages/incomes/index.tsx
@@ -1,11 +1,24 @@
+import { Typography } from "antd";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { IIncome } from "../../@types";
 import { getIncomes } from "../../api/core/Income";
 import Alert from "../../components/alert";
+import { theme } from "../../Theme";
 
 const IncomesContainer = styled.div`
   min-height: 100vh;
+`;
+
+const TitleWrapper = styled.div`
+  background-color: ${p => p.theme.colors.grays.light};
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0 5px;
+  border-radius: 10px;
 `;
 
 const Incomes = (): JSX.Element => {
@@ -30,7 +43,37 @@ const Incomes = (): JSX.Element => {
     fetchIncomes();
   }, []);
 
-  return<>hello incomes</>;
+  return(<>
+    <TitleWrapper>
+      <Typography.Text style={{
+        ...theme.texts.brandH5,
+        paddingLeft: 5
+      }}>
+        Incomes
+      </Typography.Text>
+    </TitleWrapper>
+    <IncomesContainer>
+      {(incomes || []).map(income =>
+        <Income />
+      )}
+    </IncomesContainer>
+  </>);
 };
+
+const IncomeContainer = styled.div`
+  background-color: ${p => p.theme.colors.grays.light};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  margin: 5px 0;
+  cursor: default;
+  position: relative;
+  height: 150px;
+`;
+
+const Income = (): JSX.Element => <IncomeContainer>
+
+</IncomeContainer>
 
 export default Incomes;
