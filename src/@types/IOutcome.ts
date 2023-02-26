caea7b0e7b43dd96a24da7a44d46bcb05c9248ca
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-26 14:26:04 -0600

    [CHORE]: Removed commmented types, fixed lints

--- src/@types/IOutcome.ts
+++ src/@types/IOutcome.ts
@@ -1,15 +1,5 @@
 import { ITransaction } from "./ITransaction";
 
-// export interface ICurrentOutcome extends ITransaction {
-//   transaction_type: 'current';
-// }
-
-// export interface IFixedOutcome extends ITransaction {
-//   transaction_type: 'fixed';
-// }
-
-// export type IOutcome = ICurrentOutcome | IFixedOutcome;
-
 export interface IOutcomes {
   outcomes: ITransaction [];
   total_pages: number;
