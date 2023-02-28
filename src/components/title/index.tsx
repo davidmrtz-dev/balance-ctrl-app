895fcf6767ad5ff017a5a4b7c0f74a5775d709ad
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-28 15:12:11 -0600

    [FEAT]: Extended behavior of title for incomes and outcomes pages, to handle type selector. Attaching callbacks

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Button, Popover, Typography } from "antd";
 import styled from "styled-components"
 import { theme } from "../../Theme";
 
@@ -8,11 +8,30 @@ const TitleWrapper = styled.div`
   height: 50px;
   display: flex;
   align-items: center;
-  justify-content: flex-start;
-  padding: 0 5px;
+  justify-content: space-between;
+  padding: 0 10px;
   border-radius: 10px;
   margin-bottom: 10px;
 `;
+
+const TypeOptions = (
+  <div style={{
+    display: 'flex',
+    flexDirection: 'column'
+  }}>
+    <Button style={{
+      ...theme.texts.brandSubFont,
+      marginBottom: 5
+    }}>
+      Current
+    </Button>
+    <Button style={{
+      ...theme.texts.brandSubFont
+    }}>
+      Fixed
+    </Button>
+  </div>
+);
 
 const Title = (text: string): JSX.Element => <TitleWrapper>
   <Typography.Text style={{
@@ -21,6 +40,14 @@ const Title = (text: string): JSX.Element => <TitleWrapper>
   }}>
     {text}
   </Typography.Text>
+  <Popover
+    content={TypeOptions}
+    title={`${text.slice(0, -1)} type`}
+    trigger="click"
+    overlayStyle={{ width: 150 }}
+  >
+    <Button style={{ width: 46 }}>+</Button>
+  </Popover>
 </TitleWrapper>;
 
 export default Title;
