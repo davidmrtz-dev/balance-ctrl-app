a6b65b9a3748dadbe5f46294451e31b4a7a66917
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-28 19:33:27 -0600

    [WIP]: Truncated process, adding to endpoints

--- src/components/incomes/IncomeCreate.tsx
+++ src/components/incomes/IncomeCreate.tsx
@@ -1,3 +1,20 @@
-export const IncomeCreate = (): JSX.Element => {
+import { useState } from "react";
+import { IIncome, TransactionType } from "../../@types";
+import { newIncome } from "../../generators/emptyObjects";
+
+export const IncomeCreate = ({
+  open,
+  type,
+  close,
+  handleCreate
+}: {
+  open: boolean;
+  type: TransactionType;
+  closeModal: () => void;
+  handleCreate: (income: IIncome) => Promise<void>;
+}): JSX.Element => {
+  const [loading, setLoading] = useState(false);
+  const [values, setValues] = useState<IIncome>(newIncome(type));
+
   return <>IncomeCreate</>;
 };
