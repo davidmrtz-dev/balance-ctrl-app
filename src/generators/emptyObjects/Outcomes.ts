d1b87d93c47982763e7905be18185a306471e80a
davidmrtz-dev <davidelimg1@gmail.com>
2023-02-28 18:08:37 -0600

    [FEAT]: Added new generator for incomes

--- src/generators/emptyObjects/Outcomes.ts
+++ src/generators/emptyObjects/Outcomes.ts
@@ -1,4 +1,4 @@
-import { IOutcome, TransactionType } from "../../@types";
+import { IOutcome, IIncome, TransactionType } from "../../@types";
 import dayjs from 'dayjs';
 
 export const newOutcome = <T extends TransactionType>(type: T): IOutcome => {
@@ -14,6 +14,24 @@ export const newOutcome = <T extends TransactionType>(type: T): IOutcome => {
     return newObj as IOutcome;
   } else if (type === 'fixed') {
     return { ...newObj, quotas: 3 } as IOutcome;
+  } else {
+    throw new Error(`Unsupported type: ${type}`);
+  }
+};
+
+export const newIncome = <T extends TransactionType>(type: T): IIncome => {
+  const newObj = {
+    id: 0,
+    transaction_type: type as TransactionType,
+    description: '',
+    frequency: '0',
+    amount: '1'
+  };
+
+  if (type === 'current') {
+    return newObj as IIncome;
+  } else if (type === 'fixed') {
+    return { ...newObj, frequency: '0' } as IIncome;
   } else {
     throw new Error(`Unsupported type: ${type}`);
   }
