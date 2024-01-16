bff8a77cc6915f19a872d838788caaa8629004ed
davidmrtz-dev <davidelimg1@gmail.com>
2024-01-16 17:05:42 -0600

    [WIP]: Updated and discard payment method

--- src/api/core/Billing.ts
+++ src/api/core/Billing.ts
@@ -20,3 +20,13 @@ export const createBilling = async (values: IBilling): Promise<IBilling> => {
 
   return result.data.billing;
 };
+
+export const updateBilling = async (id: number, values: IBilling): Promise<IBilling> => {
+  const result = await Http.put(`/api/v1/billings/${id}`, { billing: values }, { headers: {
+    'access-token': sessionStorage.getItem('authorization:token') || '',
+    client: sessionStorage.getItem('authorization:client') || '',
+    uid: sessionStorage.getItem('authorization:uid') || ''
+  }});
+
+  return result.data.billing;
+};
--- src/generators/emptyObjects/Billing.ts
+++ src/generators/emptyObjects/Billing.ts
@@ -1,8 +1,10 @@
 import { IBilling } from "../../@types";
 
 export const emptyBilling = (): IBilling => ({
+  id: 0,
   name: '',
   cycle_end_date: null,
   payment_due_date: null,
-  billing_type: ''
+  billing_type: '',
+  credit_card_number: ''
 });
